72a500573a48802e9813e1800c84540b6ad9f0fa
minerharry <miner.harry567@gmail.com>
2025-03-06 16:16:36 -0500

    made fetch analysis location agnostic, added push

--- gcloud/fetch_analysis.py
+++ gcloud/fetch_analysis.py
@@ -1,12 +1,21 @@
+from pathlib import Path
 import gsutilwrap
 from gsutilwrap import rsync
 
 def fetch_analysis():
-    gsutilwrap.gsutil_path = "C:/Users/Harrison Truscott/AppData/Local/Google/Cloud SDK/google-cloud-sdk/bin/gsutil.cmd"
+    gsutilwrap.gsutil_path = str(Path.home()/"AppData/Local/Google/Cloud SDK/google-cloud-sdk/bin/gsutil.cmd")
 
-    out_folder = "C:/Users/Harrison Truscott/OneDrive - University of North Carolina at Chapel Hill/Bear Lab/optotaxis calibration/data/Segmentation Analysis"
+    out_folder = str(Path.home()/"OneDrive - University of North Carolina at Chapel Hill/Bear Lab/optotaxis calibration/data/Segmentation Analysis")
 
     rsync("gs://optotaxisbucket/Segmentation Analysis",out_folder,multithreaded=True,recursive=True);
+    print("analysis sucessfully fetched");
+
+def push_analysis():
+    gsutilwrap.gsutil_path = str(Path.home()/"AppData/Local/Google/Cloud SDK/google-cloud-sdk/bin/gsutil.cmd")
+
+    out_folder = str(Path.home()/"OneDrive - University of North Carolina at Chapel Hill/Bear Lab/optotaxis calibration/data/Segmentation Analysis")
+
+    rsync(out_folder,"gs://optotaxisbucket/Segmentation Analysis",multithreaded=True,recursive=True);
     print("analysis sucessfully fetched");
 
 if __name__ == "__main__":
